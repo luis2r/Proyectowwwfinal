da38cb5f8f3637b9dfcd73f404f7daa6ee58ad82
luis <luis@luis-laptop>
2012-06-23 23:40:24 -0500

    se creo los esqueletos de el controlador torneo y sus acciones crear, liminar, modificar

---
+++ application/controllers/TorneoController.php
@@ -0,0 +1,39 @@
+<?php
+
+class TorneoController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+        /* Initialize action controller here */
+    }
+
+    public function indexAction()
+    {
+        // action body
+    }
+
+    public function crearAction()
+    {
+        // action body
+    }
+
+    public function modificarAction()
+    {
+        // action body
+    }
+
+    public function eliminarAction()
+    {
+        // action body
+    }
+
+
+}
+
+
+
+
+
+
+
