fcba07d92c01fd48aebdc60f0d7f0819b8c10878
Admin <Admin@Admin-PC>
2012-06-24 02:22:41 -0500

    Interfaz Jugador

--- application/controllers/JugadorController.php
+++ application/controllers/JugadorController.php
@@ -5,7 +5,8 @@ class JugadorController extends Zend_Controller_Action
 
     public function init()
     {
-        /* Initialize action controller here */
+        $this->initView();
+        $this->view->baseUrl = $this->_request->getBaseUrl();
     }
 
     public function indexAction()
@@ -15,7 +16,60 @@ class JugadorController extends Zend_Controller_Action
 
     public function crearAction()
     {
-        // action body
+        //titulo para la pagina
+        $this->view->title = "Agregar jugador";
+        //valor para <head><title>
+        $this->view->headTitle($this->view->title);
+        //creo el formulario
+        $form = new Application_Form_Jugadorformulario ();
+        //cambio el texto del boton submit
+        $form->submit->setLabel('Agregar jugador');
+        //lo asigno a la accion (la pag web que se mostrara)
+        $this->view->form = $form;
+        
+        //los formularios envian sus datos a traves de POST
+        //si vienen datos de post, es que el usuario ha enviado el formulario
+        if ($this->getRequest()->isPost())
+        {
+            //extrae un arreglo con los datos recibidos por POST
+            //es decir, los datos clave=>valor del formulario
+            $formData = $this->getRequest()->getPost();   
+            
+            //isValid() revisa todos los validadores y filtros que le
+            //aplicamos a los objetos del formulario: se asegura de que los
+            //campos requeridos se hallan llenado, que el formato de la fecha
+            //sea el correcto, etc
+            if ($form->isValid($formData))
+            {
+                //aca ya estamos seguros de que los datos son validos
+                //ahora los extraemos como se ve abajo
+                $jugador_id = $form->getValue('jugador_id');
+                $nombre = $form->getValue('nombre');
+                $documento = $form->getValue('documento');
+                $categoria = $form->getValue('categoria');
+                $modalidad = $form->getValue('modalidad');
+                $tipo = $form->getValue('tipo');
+                
+                //creo objeto Album que controla la talba Album de la base de datos
+                $jugadores= new Application_Model_DBTable_Jugador();
+                //llamo a la funcion agregar, con los datos que recibi del form
+                $albums->agregar($jugador_id,$nombre,$documento,$categoria,$modalidad,$tipo);
+                
+                //indico que despues de haber agregado el album,
+                //me redirija a la accion index de AlbumController, es decir,
+                //a la pagina que me muestra el listado de albumes
+                $this->_helper->redirector('crear');
+            }
+            //si los datos del formulario no son validos, es decir, falta ingresar
+            //algunos o el formato es incorrecto...
+            else
+            {
+                //esta funcion vuelve a cargar el formulario con los datos que se
+                //enviaron, Y ADEMAS CON LOS MENSAJES DE ERROR, los que se le mostrarán
+                //al usuario
+                $form->populate($formData);
+            }
+        }
     }
 
     public function modificarAction()
--- application/forms/Jugadorformulario.php
+++ application/forms/Jugadorformulario.php
@@ -2,12 +2,49 @@
 
 class Application_Form_Jugadorformulario extends Zend_Form
 {
-
     public function init()
     {
-        /* Form Elements & Other Definitions Here ... */
+        $this->setName('jugador');
+        
+        $id = new Zend_Form_Element_Hidden('id');
+        $id->addFilter($id);
+        
+        //creamos <input text> para escribir nombre del jugador
+        $nombre = new Zend_Form_Element_Text('nombre');
+        $nombre->setLabel('Nombre del jugador:')->setRequired(true)->
+                addFilter('StripTags')->addFilter('StringTrim')->
+                addValidator('NotEmpty');
+        
+        //creamos <input text> para escribir la cedula del jugador
+        $documento = new Zend_Form_Element_Text('documento');
+        $documento->setLabel('Documento del jugador:')->setRequired(true)->
+                addFilter('StripTags')->addFilter('StringTrim')->
+                addValidator('NotEmpty');
+        
+        //creamos <input text> para escribir la categoria del jugador
+        $categoria = new Zend_Form_Element_Text('categoria');
+        $categoria->setLabel('Categoria del jugador:')->setRequired(true)->
+                addFilter('StripTags')->addFilter('StringTrim')->
+                addValidator('NotEmpty');
+        
+        //creamos <input text> para escribir la modalidad del jugador
+        $modalidad = new Zend_Form_Element_Text('modalidad');
+        $modalidad->setLabel('Modalidad del jugador:')->setRequired(true)->
+                addFilter('StripTags')->addFilter('StringTrim')->
+                addValidator('NotEmpty');
+        
+        //creamos <input text> para escribir el tipo del jugador
+        $tipo = new Zend_Form_Element_Text('tipo');
+        $tipo->setLabel('Tipo del jugador:')->setRequired(true)->
+                addFilter('StripTags')->addFilter('StringTrim')->
+                addValidator('NotEmpty');
+        
+        //boton para enviar formulario
+        $submit = new Zend_Form_Element_Submit('submit');
+        $submit->setAttrib('id', 'submitbutton');
+        
+        //agrego los objetos creados al formulario
+        $this->addElements(array($id,$nombre,$documento,$categoria,$modalidad,$tipo,submit));
     }
-
-
 }
 
