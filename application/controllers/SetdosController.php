858185c82e42e62dfcf4f31ec1e32eb9fe8bf3d7
palacios <palacios@palacios-MS-7374>
2012-07-01 18:04:10 -0500

    GUI's de acceso mobil con controlador por set, modificarmobil.phtml  llama a partidofinal y a partidonorm, desde donde partidofinal llama a los controladores de cada set (1 2 3)

---
+++ application/controllers/SetdosController.php
@@ -0,0 +1,32 @@
+<?php
+
+class SetdosController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+        /* Initialize action controller here */
+    }
+
+    public function indexAction()
+    {$this->_helper->layout()->disableLayout(); 
+        // action body
+    }
+
+    public function modificarAction()
+    {
+        // action body
+    }
+
+    public function crearAction()
+    {
+        // action body
+    }
+
+
+}
+
+
+
+
+
